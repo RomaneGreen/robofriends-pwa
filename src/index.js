6424671fd5249d0ca20a9e45e4b2204bce1fc4ff
Romane G <romane71193@gmail.com>
2019-07-09 18:07:51 -0400

    test2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import 'tachyons';
 
-      import App from './containers/App';
+    import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
